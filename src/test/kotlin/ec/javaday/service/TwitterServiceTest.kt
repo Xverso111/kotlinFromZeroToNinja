7b8124648f4e7b52f4ff63ffc99b775fc7656c3a
Mateo Rojas <mateo.rojas@thoughtworks.com>
2019-08-13 10:45:00 -0500

    Fixes non-compiling test

--- src/test/kotlin/ec/javaday/service/TwitterServiceTest.kt
+++ src/test/kotlin/ec/javaday/service/TwitterServiceTest.kt
@@ -2,6 +2,7 @@ package ec.javaday.service
 
 import ec.javaday.domain.SearchCriteria
 import ec.javaday.repository.SearchCriteriaRepository
+import ec.javaday.twitter.TwitterClient
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Test
@@ -11,12 +12,13 @@ class TwitterServiceTest {
     @Test
     fun `should insert provided search criteria`() {
         //mockk libreria para dobles de prueba
+        val twitterClient = mockk<TwitterClient>()
         val searchCriteriaRepository =
             mockk<SearchCriteriaRepository>(relaxed = true)
 
         val searchCriteria = mockk<SearchCriteria>()
 
-        val twitterService = TwitterService(searchCriteriaRepository)
+        val twitterService = TwitterService(searchCriteriaRepository, twitterClient)
 
         twitterService.createSearchCriteria(searchCriteria)
 
